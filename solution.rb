5b82bfbe769932797a6faf6691af8b2a5f7f67ee
tatavosilva <tatavosilva@users.noreply.github.com>
2019-06-16 21:14:07 -0500

    ejercicio 2 utilizando path3

--- solution.rb
+++ solution.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
-get '/' do
-  if params[:nombre] == "Juan" 
+get '/makers/:nombre' do
+  if params[:nombre] == "juan" 
    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   else
    "<h1>Hola desconocido!</h1>"
